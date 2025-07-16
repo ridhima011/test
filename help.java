9798b3c2062bc93694490455e82c51c03c691ac2
ridhima011 <122424799+ridhima011@users.noreply.github.com>
2025-07-16 09:55:11 +0530

    Update help.java

--- help.java
+++ help.java
@@ -1,5 +1,5 @@
 
 class help{
   public static void main(String[] args) {
-        System.out.println("namaste ajj se swan shuru");
+        System.out.println("namaste ajj se swan shuru !!!!");
   }}
